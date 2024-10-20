16e0323bd67d18dcf995b0c66fb15ae7167e3837
M-Marvin <69386758+M-Marvin@users.noreply.github.com>
2024-10-20 02:16:27 +0200

    Cleaned up project folders

--- IndustriaCore-1.20/src/main/java/de/m_marvin/industria/core/Config.java
+++ IndustriaCore-1.20/src/main/java/de/m_marvin/industria/core/Config.java
@@ -34,7 +34,7 @@ public class Config {
 		EF_DEBUG_LOGGING = BUILDER.comment("If true, the native lib will print simmulation data (and some other things) from the electric networks into the logs.").define("simulation_debug_logging", false);
 		BUILDER.pop();
 		BUILDER.comment("Industria Core electrics settings").push(CATEGORY_ELECTIRCS);
-		ELECTIRC_SIMULATION_THREADS = BUILDER.comment("Number of paralell running EPT's").defineInRange("simulation_threads", 2, 1, 16);
+		ELECTIRC_SIMULATION_THREADS = BUILDER.comment("Number of paralell running EPT's").defineInRange("simulation_threads", 1, 1, 16);
 		ELECTRIC_SIMULATION_COMMAND = BUILDER.comment("Simulation engine execution command.").define("simulation_exec_command", "op");
 		CONFIG_COMMON = BUILDER.build();
 		
